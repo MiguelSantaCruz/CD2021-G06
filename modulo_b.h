2d2fb8b94fe1f82cdcee62ba6f40a131c42687e5
StOnEOP <jonasm10a@gmail.com>
2020-12-24 01:51:16 +0000

    Mais algum progresso

--- modulo_b.h
+++ modulo_b.h
@@ -8,7 +8,18 @@
 #include "modulo_a.h"
 #include "modulo_c.h"
 #include "modulo_d.h"
-#include "fsize.h"
+
+typedef struct node {
+    int init_symbol; //109
+    int final_symbol;
+    int freq; // 768
+    int code; // 10
+}node;
+
 void readFreqs (FILE *freq, int *indexPointer, char *buffer, long long block_size);
+
+int distArroba (FILE *freq, int size, int x);
+
+void toStruct (struct node nodes[256], char *buffer, long long n_blocks, char *freq_Array);
 
 #endif
