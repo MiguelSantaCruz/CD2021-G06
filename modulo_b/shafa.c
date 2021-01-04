80be7599a7da44326931573a99d02a5f1a3241e4
StOnEOP <jonasm10a@gmail.com>
2021-01-04 02:48:17 +0000

    Ultimo commit v2

--- modulo_b/shafa.c
+++ modulo_b/shafa.c
@@ -1,3 +1,11 @@
+/*
+Autores: João Nunes - A87972 , André Silva - A87958
+Data: 01/01/2020
+Versão: Final
+
+Este ficheiro contem a função main do módulo B.
+*/
+
 #include "modulo_b.c"
 
 void main (int argc, char *argv[]){
